71c39737f29a23871142f83f7a1a6003f6d494c0
Lucas Lopes <medowlucas@gmail.com>
2022-01-17 23:35:06 -0300

    criando rotas

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send("Olá Mundo!");
+    res.send("Olá Mundo 2");
+})
+
+app.get('/contato', (req, res)=>{
+    res.send("Página de contato 4");
+})
+
+app.get('/sobre', (req, res)=>{
+    res.send("Página de sobre 4");
+})
+
+app.get('/mensagem', (req, res)=>{
+    res.send("Página de mensagem 4");
 })
 
 const PORT = 8080;
